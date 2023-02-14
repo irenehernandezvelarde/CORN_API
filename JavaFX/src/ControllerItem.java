efa055cfb8eddf5b4ef6812a820c5db3708d15b7
pmunuera <95918506+pmunuera@users.noreply.github.com>
2023-02-14 18:24:41 +0100

    First Commit

---
+++ JavaFX/src/ControllerItem.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Polygon;
+
+public class ControllerItem {
+    
+    @FXML
+    private Label title, subtitle;
+
+    @FXML
+    private Polygon coloredShape;
+
+    @FXML
+    private void handleMenuAction() {
+        Controller0 c0 = (Controller0) UtilsViews.getController("View0");
+        c0.loadConsoleInfo(title.getText());
+    }
+
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle.setText(subtitle);
+    }
+
+    public void setColor(String color) {
+        coloredShape.setStyle("-fx-fill: " + color);
+    }
+}
